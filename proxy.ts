ddd0a2c4ef319935ef654cd7a2eaf79ba90e4376
Faris hamad <bluocat4@gmail.com>
2026-01-15 13:59:05 +0200

    test

--- proxy.ts
+++ proxy.ts
@@ -22,7 +22,7 @@ export const config = {
   /*
    * Match all request paths except for the ones starting with:
    * - api (API routes)
-   * - _next/static (static files)
+   * - _next/static (static files
    * - _next/image (image optimization files)
    * - favicon.ico (favicon file)
    * - public (public folder assets like logo.png)
